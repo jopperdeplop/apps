bee8b61f4f598eee79fff59e43b8ccf51474b81f
513609 <513609jb@eur.nl>
2025-11-19 00:47:19 +0100

    Update saleor-app.ts

--- apps/stripe/src/lib/saleor-app.ts
+++ apps/stripe/src/lib/saleor-app.ts
@@ -1,11 +1,11 @@
+/* eslint-disable n/no-process-env */
 import { APL } from "@saleor/app-sdk/APL";
 import { DynamoAPL } from "@saleor/app-sdk/APL/dynamodb";
 import { FileAPL } from "@saleor/app-sdk/APL/file";
-import { UpstashAPL } from "@saleor/app-sdk/APL"; // Import UpstashAPL
+import { UpstashAPL } from "@saleor/app-sdk/APL/upstash";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
 
 import { dynamoMainTable } from "@/modules/dynamodb/dynamo-main-table";
-
 import { env } from "./env";
 
 export let apl: APL;
@@ -15,13 +15,10 @@ switch (env.APL) {
     apl = DynamoAPL.create({
       table: dynamoMainTable,
     });
-
     break;
   }
   
   case "upstash": {
-    // We use process.env here to ensure it works even if you haven't 
-    // fully updated the strict Zod validation schema in env.ts yet.
     apl = new UpstashAPL({
       restURL: process.env.UPSTASH_REDIS_REST_URL!,
       restToken: process.env.UPSTASH_REDIS_REST_TOKEN!,
