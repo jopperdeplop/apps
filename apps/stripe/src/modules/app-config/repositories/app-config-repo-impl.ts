911536a15b879634a8e0b0005fe78771455f6618
513609 <513609jb@eur.nl>
2025-11-19 01:57:16 +0100

    Update app-config-repo-impl.ts

--- apps/stripe/src/modules/app-config/repositories/app-config-repo-impl.ts
+++ apps/stripe/src/modules/app-config/repositories/app-config-repo-impl.ts
@@ -1,10 +1,6 @@
-// app-config-repo-impl.ts
-
-// 1. Import the Redis implementation instead of DynamoDB
-import { RedisAppConfigRepo } from "@/modules/app-config/repositories/redis/redis-app-config-repo"; 
+import { DynamodbAppConfigRepo } from "@/modules/app-config/repositories/dynamodb/dynamodb-app-config-repo";
 
 /*
  * Replace this implementation with custom DB (Redis, Metadata etc) to drop DynamoDB and bring something else
  */
-// 2. Instantiate and export the Redis repository
-export const appConfigRepoImpl = new RedisAppConfigRepo();
+export const appConfigRepoImpl = new DynamodbAppConfigRepo();
