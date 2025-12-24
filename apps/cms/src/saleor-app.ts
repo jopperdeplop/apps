1022f0fd07e2e7a553ba962a46bb90828454b440
513609 <513609jb@eur.nl>
2025-12-24 12:53:05 +0100

    fix(cms): map Vercel KV env vars to UpstashAPL and declare in turbo.json

--- apps/cms/src/saleor-app.ts
+++ apps/cms/src/saleor-app.ts
@@ -37,7 +37,16 @@ switch (aplType) {
   }
 
   case "upstash":
-    apl = new UpstashAPL();
+    apl = new UpstashAPL({
+      restURL:
+        process.env.UPSTASH_URL ||
+        process.env.UPSTASH_REDIS_REST_URL ||
+        process.env.KV_REST_API_URL!,
+      restToken:
+        process.env.UPSTASH_TOKEN ||
+        process.env.UPSTASH_REDIS_REST_TOKEN ||
+        process.env.KV_REST_API_TOKEN!,
+    });
 
     break;
 
